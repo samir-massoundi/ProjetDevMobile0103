864a351ab3e81cb2d67c7537c31e9de80901aa23
Samir Massoundi <samir.massoundi@ynov.com>
2021-03-01 10:37:50 +0100

    updated home fragment, biscuits eaten

--- app/src/main/java/fr/samir/projetDevMob/MainActivity.kt
+++ app/src/main/java/fr/samir/projetDevMob/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.samir.projetDevMob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.samir.projetDevMob.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Injection du fragment
+        val transact= supportFragmentManager.beginTransaction()
+        transact.replace(R.id.fragmentContainer, HomeFragment())
+        transact.addToBackStack(null)
+        transact.commit()
     }
 }
